c72b420a472992843894d07da17f4a2d3090ffeb
JensVanAssche <jens.va@hotmail.com>
2018-10-16 21:50:08 +0200

    homepage structure and scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,64 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Nutella Wedstrijd</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -76,19 +22,28 @@
                     @endauth
                 </div>
             @endif
+        </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <div class="container home">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center">Nutella's Grote Wedstrijd</h1>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="/images/jar.png" class="img-fluid">
+                </div>
+                <div class="col">
+                    <p>uitleg over de wedstrijd</p>
+                    <a href="#" class="btn btn-primary">Show me more!</a>
+                </div>
+                <div class="col">
+                    <h2>Prijzen</h2>
+                    <p>prijzen hier</p>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <h2>Winnaars</h2>
+                    <p>winnaars hier</p>
                 </div>
             </div>
         </div>
